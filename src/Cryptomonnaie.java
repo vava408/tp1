8b6ff07f6e16629bcf920e2f6a00544da332a6f6
vava408 <valentinlemaitre06@gmail.com>
2025-04-01 19:40:59 +0200

    Essaie du pull

--- src/Cryptomonnaie.java
+++
@@ -1,9 +0,0 @@
-public class Cryptomonnaie{
-    private String nom;
-    private double valeurDeJeton; // Imaginons en euros
-
-    public Cryptomonnaie(String nom, double valeurDeJeton){
-        this.nom = nom;
-        this.valeurDeJeton = valeurDeJeton;
-    }
-}
