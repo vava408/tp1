8d9db769b8cdd9c32e6ef4efb8a8c39f2f030aee
vava408 <valentinlemaitre06@gmail.com>
2025-04-01 19:41:25 +0200

    Message pour le log

---
+++ src/Cryptomonnaie.java
@@ -0,0 +1,9 @@
+public class Cryptomonnaie{
+    private String nom;
+    private double valeurDeJeton; // Imaginons en euros
+
+    public Cryptomonnaie(String nom, double valeurDeJeton){
+        this.nom = nom;
+        this.valeurDeJeton = valeurDeJeton;
+    }
+}
